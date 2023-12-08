13fa9200cdb20d8cf4598c1e925f7c0f04cf315c
udbhavbalaji <udbhavbalaji@gmail.com>
2023-12-08 12:52:36 -0500

    Added unit tests for left face transformation

--- Tests.py
+++ Tests.py
@@ -1,14 +1,114 @@
 import unittest
 from CubeInit import Cube, Face, Piece
+from Constraints import FacePositions, Axes
+from Transformations import FaceTransformations, GridTransformations
 
 class TestValidity(unittest.TestCase):
     
     def test_cube_init(self):
-        self.assertEqual(Cube().current_perspective.opposite.center_colour, Cube().green_face.center_colour)
+        cube = Cube()
+        self.assertEqual(cube.current_perspective.left.right, cube.current_perspective)
+        self.assertEqual(cube.current_perspective.top.back, cube.current_perspective.opposite)
+        self.assertEqual(cube.current_perspective.bottom.front, cube.current_perspective)
+            
+    def test_opposite_faces(self):
+        cube = Cube()
+        self.assertEqual(cube.current_perspective.opposite, cube.green_face)
+        self.assertEqual(cube.current_perspective.left, cube.red_face)
+        self.assertEqual(cube.current_perspective.right, cube.orange_face)
+        self.assertEqual(cube.current_perspective.top, cube.white_face)
+        self.assertEqual(cube.current_perspective.bottom, cube.yellow_face)
+        self.assertEqual(cube.current_perspective, cube.blue_face)
+
+    def test_left_face_transformation_rotate_down(self):
+        cube = Cube()
+        
+        #$ Testing transformation when rotating down
+        new_left_face = FaceTransformations.UpAndDown.transform_left_face(cube.current_perspective, is_rotate_down=True)
+        self.assertEqual(new_left_face.top, cube.current_perspective.opposite)
+        self.assertEqual(new_left_face.right, cube.current_perspective.top)
+        self.assertEqual(new_left_face.opposite, cube.current_perspective.right)
+    
+    def test_left_face_transformation_rotate_up(self):
+        cube = Cube()
+        
+        #$ Testing transformation when rotating up
+        new_left_face = FaceTransformations.UpAndDown.transform_left_face(cube.current_perspective, is_rotate_down=False)
+        self.assertEqual(new_left_face.top, cube.current_perspective)
+        self.assertEqual(new_left_face.right, cube.current_perspective.bottom)
+        self.assertEqual(new_left_face.opposite, cube.current_perspective.right)
+        
+    def test_left_face_transformation_rotate_left_horizontal(self):
+        cube = Cube()
+        
+        #$ Testing transformation when rotating left horizontally
+        new_left_face = FaceTransformations.LeftAndRight.transform_left_face(cube.current_perspective, is_left=True)
+        self.assertEqual(new_left_face.left, cube.current_perspective.top)
+        self.assertEqual(new_left_face.front, cube.current_perspective)
+        self.assertEqual(new_left_face.opposite, cube.current_perspective.right)
+        
+    def test_left_face_transformation_rotate_right_horizontal(self):
+        cube = Cube()
+        
+        #$ Testing transformation when rotating right horizontally
+        new_left_face = FaceTransformations.LeftAndRight.transform_left_face(cube.current_perspective, is_left=False)
+        self.assertEqual(new_left_face.left, cube.current_perspective.bottom)
+        self.assertEqual(new_left_face.front, cube.current_perspective)
+        self.assertEqual(new_left_face.opposite, cube.current_perspective.right)
+        
+    def test_left_face_transformation_invert_cube_horizontal(self):
+        cube = Cube()
+        #$ Testing transformation when inverting cube horizontally
+        new_left_face = FaceTransformations.Inversion.transform_right_left_faces(cube.current_perspective, is_left_face=True)
+        self.assertEqual(new_left_face.top, cube.current_perspective.bottom)
+        self.assertEqual(new_left_face.opposite, cube.current_perspective.right)
+        self.assertEqual(new_left_face.right, cube.current_perspective.opposite)
         pass
+        
+    # def test_right_face_transformation_rotate_down(self):
+    #     cube = Cube()
+        
+    #     #$ Testing transformation when rotating down
+    #     new_right_face = FaceTransformations.UpAndDown.transform_right_face(cube.current_perspective, is_rotate_down=True)
+    #     self.assertEqual(new_right_face.top, cube.current_perspective.opposite)
+    #     self.assertEqual(new_right_face.right, cube.current_perspective.bottom)
+    #     self.assertEqual(new_right_face, cube.current_perspective.right)
     
-    
-    pass
+    # def test_right_face_transformation_rotate_up(self):
+    #     cube = Cube()
+        
+    #     #$ Testing transformation when rotating up
+    #     new_right_face = FaceTransformations.UpAndDown.transform_right_face(cube.current_perspective, is_rotate_down=False)
+    #     self.assertEqual(new_right_face.top, cube.current_perspective.opposite)
+    #     self.assertEqual(new_right_face.right, cube.current_perspective.top)
+    #     self.assertEqual(new_right_face, cube.current_perspective.right)
+        
+    # def test_right_face_transformation_rotate_left_horizontal(self):
+    #     cube = Cube()
+        
+    #     #$ Testing transformation when rotating right horizontally
+    #     new_right_face = FaceTransformations.rightAndRight.transform_right_face(cube.current_perspective, is_left=True)
+    #     self.assertEqual(new_right_face.right, cube.current_perspective.top)
+    #     self.assertEqual(new_right_face.front, cube.current_perspective)
+    #     self.assertEqual(new_right_face.opposite, cube.current_perspective.right)
+        
+    # def test_right_face_transformation_rotate_right_horizontal(self):
+    #     cube = Cube()
+        
+    #     #$ Testing transformation when rotating right horizontally
+    #     new_right_face = FaceTransformations.rightAndRight.transform_right_face(cube.current_perspective, is_left=False)
+    #     self.assertEqual(new_right_face.right, cube.current_perspective.bottom)
+    #     self.assertEqual(new_right_face.front, cube.current_perspective)
+    #     self.assertEqual(new_right_face.opposite, cube.current_perspective.right)
+        
+    # def test_right_face_transformation_invert_cube_horizontal(self):
+    #     cube = Cube()
+    #     #$ Testing transformation when inverting cube horizontally
+    #     new_right_face = FaceTransformations.Inversion.transform_right_right_faces(cube.current_perspective, is_left_face=True)
+    #     self.assertEqual(new_right_face.top, cube.current_perspective.right)
+    #     self.assertEqual(new_right_face.front, cube.current_perspective)
+    #     self.assertEqual(new_right_face.opposite, cube.current_perspective.right)
+        
 
 
 if __name__ == '__main__':
